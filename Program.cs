4a21580d1c1dc8e6140ca07e1b715f8c31d9063c
Ammon Johnson <ammonsawesome@gmail.com>
2021-07-17 15:21:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiceRollerWithEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
